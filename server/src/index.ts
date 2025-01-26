226e1edf1c1aa6dc660acffbb1b982cb71c3d9c2
iamrohitg <rohitghosh76@gmail.com>
2025-01-27 00:26:34 +0530

    Add project endpoint's.

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,6 +16,7 @@ import authRoutes from "./routes/auth.route";
 import userRoutes from "./routes/user.route";
 import isAuthenticated from "./middlewares/isAuthenticated.middleware";
 import workspaceRoutes from "./routes/workspace.route";
+import projectRoutes from "./routes/project.route";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -72,6 +73,7 @@ app.use(`${BASE_PATH}/auth`, authRoutes);
 
 app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
 app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
+app.use(`${BASE_PATH}/project`, isAuthenticated, projectRoutes);
 
 app.use(errorHandler);
 
