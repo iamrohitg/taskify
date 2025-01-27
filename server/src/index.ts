076a6138dc1e05dfac047288302b467d2d40bfdf
iamrohitg <rohitghosh76@gmail.com>
2025-01-27 12:40:23 +0530

    Add member endpoint.

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,6 +17,7 @@ import userRoutes from "./routes/user.route";
 import isAuthenticated from "./middlewares/isAuthenticated.middleware";
 import workspaceRoutes from "./routes/workspace.route";
 import projectRoutes from "./routes/project.route";
+import memberRoutes from "./routes/member.route";
 
 const app = express();
 const BASE_PATH = config.BASE_PATH;
@@ -73,6 +74,7 @@ app.use(`${BASE_PATH}/auth`, authRoutes);
 
 app.use(`${BASE_PATH}/user`, isAuthenticated, userRoutes);
 app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
+app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
 app.use(`${BASE_PATH}/project`, isAuthenticated, projectRoutes);
 
 app.use(errorHandler);
