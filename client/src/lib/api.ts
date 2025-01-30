516606ce2e319b5e32723f91e27db47c640d0a20
iamrohitg <rohitghosh76@gmail.com>
2025-01-30 21:01:12 +0530

    Add auth provider and proctected routes.

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -7,6 +7,7 @@ import {
   LoginResponseType,
   loginType,
   registerType,
+  WorkspaceByIdResponseType,
 } from "@/types/api.type";
 
 export const loginMutationFn = async (
@@ -39,7 +40,12 @@ export const createWorkspaceMutationFn = async (
 
 export const editWorkspaceMutationFn = async () => {};
 
-export const getWorkspaceByIdQueryFn = async () => {};
+export const getWorkspaceByIdQueryFn = async (
+  workspaceId: string
+): Promise<WorkspaceByIdResponseType> => {
+  const response = await API.get(`/workspace/${workspaceId}`);
+  return response.data;
+};
 
 export const getAllWorkspacesUserIsMemberQueryFn =
   async (): Promise<AllWorkspaceResponseType> => {
