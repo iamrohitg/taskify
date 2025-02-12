9341e86d79177523ea8d935cd472b5f15e5395d7
iamrohitg <rohitghosh76@gmail.com>
2025-02-12 16:52:08 +0530

    Fix: Add zustand and fix auth 401 error.

--- client/src/routes/common/routes.tsx
+++ client/src/routes/common/routes.tsx
@@ -1,4 +1,3 @@
-import GoogleOAuthFailure from "@/page/auth/GoogleOAuthFailure";
 import SignIn from "@/page/auth/Sign-in";
 import SignUp from "@/page/auth/Sign-up";
 import WorkspaceDashboard from "@/page/workspace/Dashboard";
@@ -8,11 +7,12 @@ import Settings from "@/page/workspace/Settings";
 import Tasks from "@/page/workspace/Tasks";
 import { AUTH_ROUTES, BASE_ROUTE, PROTECTED_ROUTES } from "./routePaths";
 import InviteUser from "@/page/invite/InviteUser";
+import GoogleOAuth from "@/page/auth/GoogleOAuth";
 
 export const authenticationRoutePaths = [
   { path: AUTH_ROUTES.SIGN_IN, element: <SignIn /> },
   { path: AUTH_ROUTES.SIGN_UP, element: <SignUp /> },
-  { path: AUTH_ROUTES.GOOGLE_OAUTH_CALLBACK, element: <GoogleOAuthFailure /> },
+  { path: AUTH_ROUTES.GOOGLE_OAUTH_CALLBACK, element: <GoogleOAuth /> },
 ];
 
 export const protectedRoutePaths = [
